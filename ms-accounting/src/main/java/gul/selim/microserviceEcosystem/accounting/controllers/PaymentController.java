01b9daa305051dc70f19b9744165ec86cda2a676
selimgul <selimgulce@gmail.com>
2021-03-10 18:01:15 +0300

    .

--- ms-accounting/src/main/java/gul/selim/microserviceEcosystem/accounting/controllers/PaymentController.java
+++ ms-accounting/src/main/java/gul/selim/microserviceEcosystem/accounting/controllers/PaymentController.java
@@ -2,18 +2,26 @@ package gul.selim.microserviceEcosystem.accounting.controllers;
 
 import gul.selim.microserviceEcosystem.accounting.models.PaymentRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/payment")
+@RefreshScope
 public class PaymentController {
+
+    @Value("${config-key:}")
+    private String configKey;
 
     @Value("${server.port}")
     private Integer port;
+
+    @GetMapping("/pconfig")
+    public String logConfig() {
+        System.out.println(configKey);
+        return "Config values logged to console.";
+    }
 
     @PostMapping("/pay")
     public String pay(@Validated @RequestBody final PaymentRequest pr) {
--- ms-order/src/main/java/gul/selim/microserviceEcosystem/order/controllers/OrderController.java
+++ ms-order/src/main/java/gul/selim/microserviceEcosystem/order/controllers/OrderController.java
@@ -4,10 +4,12 @@ import gul.selim.microserviceEcosystem.order.models.Order;
 import gul.selim.microserviceEcosystem.order.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderController {
 
     @Value("${config-key:}")
