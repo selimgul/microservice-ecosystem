1747309ad32e03f6ba0e509ca9354d67648198a8
selimgul <selimgul>
2021-03-17 17:52:56 +0300

    .

---
+++ scripts/compile.bat
@@ -0,0 +1,9 @@
+
+start mvn clean install -f ../ms-accounting/pom.xml
+start mvn clean install -f ../ms-notification/pom.xml
+start mvn clean install -f ../ms-order/pom.xml
+start mvn clean install -f ../node-apigateway/pom.xml
+start mvn clean install -f ../node-configserver/pom.xml
+start mvn clean install -f ../node-discovery/pom.xml
+
+
